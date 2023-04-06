729204d83348d78de993a00a3bcb5c33fd8de81e
abdullah <abdullahmsd85@gmail.com>
2023-04-07 02:33:13 +0500

    a
    a

--- employee_details.php
+++ employee_details.php
@@ -1,125 +1,157 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-   <?php include ('link/links.php')?>
+  <?php include('link/links.php') ?>
+
 </head>
+
 <body>
   <div id="main">
-    <?php include('link/desigene/navbar.php')?>
+    <?php include('link/desigene/navbar.php') ?>
     <div class="container-fluid m-auto py-5">
-      <div class="row">
-        <!-- left column -->
-        <div class="col-md-12">
-          <div class="card card-success">
-            <div style="background-color: darkblue;" class="card-header  text-white fw-bold">
-              <div class="card-title">Leave Request</div>
-            </div>
-            <!-- /.card-header -->
-            <div class="card-body bg-light">
-              <!-- form start -->
-              <form method="post" enctype="multipart/form-data">
-                <div class="row">
-                  <div class="col-4">
-                    <div class="form-group">
-                      <label>Employee No </label>
-                      <input type="text" name="" placeholder="Employee No" class="form-control" autocomplete="off" required="">
+      <div class="row" id="emp_card">
+        <div class="content">
+          <section class="content">
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-md-3">
+                  <div class="card card-primary card-outline">
+                    <div class="card-body box-profile">
+                      <div class="text-center">
+                        <img class="profile-user-img img-fluid img-circle" src="image/image-removebg-preview.png" alt="User profile picture">
+                      </div>
+                      <h3 class="profile-username text-center">Sharafat Ali Khan</h3>
+                      <ul class="list-group list-group-unbordered mb-3">
+                        <li class="list-group-item">
+                          <b>Employee Id:</b> <b class="float-right">01</b>
+                        </li>
+                        <li class="list-group-item">
+                          <b>Source:</b> <b class="float-right">WSSC</b>
+                        </li>
+                        <li class="list-group-item">
+                          <div class="form-check">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                            <label class="form-check-label" for="flexRadioDefault1">
+                              Double Duty
+                            </label>
+                          </div>
+                          <div class="form-check">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                            <label class="form-check-label" for="flexRadioDefault2">
+                              Over Time
+                            </label>
+                          </div>
+                        </li>
+                      </ul>
+                      <a href="#" class="btn btn-primary btn-block"><b>Employee Details</b></a>
                     </div>
                   </div>
+                </div>
+                <div class="col-md-3">
+                  <div class="card card-primary card-outline">
+                    <div class="card-body box-profile">
+                      <div class="text-center">
+                        <img class="profile-user-img img-fluid img-circle" src="image/image-removebg-preview.png" alt="User profile picture">
+                      </div>
+                      <h3 class="profile-username text-center">Sharafat Ali Khan</h3>
+                      <ul class="list-group list-group-unbordered mb-3">
+                        <li class="list-group-item">
+                          <b>Employee Id:</b> <b class="float-right">01</b>
+                        </li>
+                        <li class="list-group-item">
+                          <b>Source:</b> <b class="float-right">WSSC</b>
+                        </li>
+                        <li class="list-group-item">
+                          <div class="form-check">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                            <label class="form-check-label" for="flexRadioDefault1">
+                              Double Duty
+                            </label>
+                          </div>
+                          <div class="form-check">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                            <label class="form-check-label" for="flexRadioDefault2">
+                              Over Time
+                            </label>
+                          </div>
+                        </li>
+                      </ul>
+                      <a href="#" class="btn btn-primary btn-block"><b>Employee Details</b></a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-3">
+                  <div class="card card-primary card-outline">
+                    <div class="card-body box-profile">
+                      <div class="text-center">
+                        <img class="profile-user-img img-fluid img-circle" src="image/image-removebg-preview.png" alt="User profile picture">
+                      </div>
+                      <h3 class="profile-username text-center">Sharafat Ali Khan</h3>
+                      <ul class="list-group list-group-unbordered mb-3">
+                        <li class="list-group-item">
+                          <b>Employee Id:</b> <b class="float-right">01</b>
+                        </li>
+                        <li class="list-group-item">
+                          <b>Source:</b> <b class="float-right">WSSC</b>
+                        </li>
+                        <li class="list-group-item">
+                          <div class="form-check">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                            <label class="form-check-label" for="flexRadioDefault1">
+                              Double Duty
+                            </label>
+                          </div>
+                          <div class="form-check">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                            <label class="form-check-label" for="flexRadioDefault2">
+                              Over Time
+                            </label>
+                          </div>
+                        </li>
+                      </ul>
+                      <a href="#" class="btn btn-primary btn-block"><b>Employee Details</b></a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-3">
+                  <div class="card card-primary card-outline">
+                    <div class="card-body box-profile">
+                      <div class="text-center">
+                        <img class="profile-user-img img-fluid img-circle" src="image/image-removebg-preview.png" alt="User profile picture">
+                      </div>
+                      <h3 class="profile-username text-center">Sharafat Ali Khan</h3>
+                      <ul class="list-group list-group-unbordered mb-3">
+                        <li class="list-group-item">
+                          <b>Employee Id:</b> <b class="float-right">01</b>
+                        </li>
+                        <li class="list-group-item">
+                          <b>Source:</b> <b class="float-right">WSSC</b>
+                        </li>
+                        <li class="list-group-item">
+                          <div class="form-check">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                            <label class="form-check-label" for="flexRadioDefault1">
+                              Double Duty
+                            </label>
+                          </div>
+                          <div class="form-check">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                            <label class="form-check-label" for="flexRadioDefault2">
+                              Over Time
+                            </label>
+                          </div>
+                        </li>
+                      </ul>
+                      <a href="#" class="btn btn-primary btn-block"><b>Employee Details</b></a>
+                    </div>
+                  </div>
+                </div>
 
-                  <div class="col-4">
-                    <div class="form-group">
-                      <label>Name </label>
-                      <input type="text" name="" placeholder="Name" class="form-control" autocomplete="off" required="">
-                    </div>
-                  </div>
-                  <div class="col-4">
-                    <div class="form-group">
-                      <label>Request No</label>
-                      <input type="text" name="" placeholder="Request No" class="form-control" autocomplete="off" required="">
-                    </div>
-                  </div>  
-                  <div class="col-4">
-                    <div class="form-group">
-                      <label>Request Date</label>
-                      <input type="date" name="" placeholder="Request Date" class="form-control" autocomplete="off" required="">
-                    </div>
-                  </div>
-                  <div class="col-4">
-                    <div class="form-group">
-                      <label>From City</label>
-                      <input type="text" name="" placeholder="From City" class="form-control" autocomplete="off" required="">
-                    </div>
-                  </div>
-                  <div class="col-4">
-                    <div class="form-group">
-                      <label>To City</label>
-                      <input type="text" name="" placeholder="To City" class="form-control" autocomplete="off" required="">
-                    </div>
-                  </div>
-                  <div class="col-4">
-                    <div class="form-group">
-                      <label>Departure ON</label>
-                      <input type="date" name="" placeholder="Departure ON" class="form-control" autocomplete="off" required="">
-                    </div>
-                  </div>
-                  <div class="col-4">
-                    <div class="form-group">
-                      <label>Return Date</label>
-                      <input type="date" name="" placeholder="" class="form-control" autocomplete="off" required="">
-                    </div>
-                  </div>
-                  <div class="col-4">
-                    <div class="form-group">
-                      <label>Travel Mode</label>
-                      <input type="text" name="" placeholder="Travel Mode" class="form-control" autocomplete="off" required="">
-                    </div>
-                  </div>
-                  <div class="col-4">
-                    <div class="form-group">
-                      <label>Justification</label>
-                      <textarea class="form-control"></textarea>
-                    </div>
-                  </div>
-                  <div class="col-md-12 text-end mt-2">
-                    <input style="background-color: darkblue;" type="submit" class="btn text-white float-right shadow" value="Submit" name="saveUser1">
-                  </div>
-                </div>
-              </form>
-              <div class="row my-3">
-                <div class="col-12">
-                  <table class="table">
-                    <thead class="text-white" style="background-color: darkblue;">
-                      <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Date</th>
-                        <th scope="col" colspan="4">Travel Description</th>
-                        <th scope="col">Req,Status</th>
-                        <th scope="col">Bill Status</th>
-                        <th scope="col">Add TA Bill</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <th scope="row">1</th>
-                        <td></td>
-                        <td colspan="4"></td>
-                        <td></td>
-                        <td></td>
-                        <td><a href="Tabil.php"><i class="fa-solid fa-file-invoice-dollar"></i></a></td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
-            <!-- /.card-body -->
-          </div>
-          <!-- /.card -->
+
+                  
         </div>
-        <!-- Col-12 -->
-      </div>
-    </div>
-  </div>
-  <?php include('link/desigene/script.php')?>
+        <?php include('link/desigene/script.php') ?>
 </body>
+
 </html>
