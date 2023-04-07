4418528dd80f0f4d600625ca2c656afbb00f76ec
abdullah <abdullahmsd85@gmail.com>
2023-04-08 01:27:44 +0500

    4 / 4/23

--- admaindash/link/links.php
+++ admaindash/link/links.php
@@ -27,6 +27,8 @@
     <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
     <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <script src="https://cdn.ckeditor.com/4.19.1/standard/ckeditor.js"></script>
     <script src="https://kit.fontawesome.com/49d698e135.js" crossorigin="anonymous"></script>
